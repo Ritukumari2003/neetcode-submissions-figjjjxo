2c1b1ea6376accfe60759fd2c0dd71dbdfc37730
RITU KUMARI <118442493+Ritukumari2003@users.noreply.github.com>
2026-04-03 13:36:44 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Brute Force Approach
+        n = len(nums)
+        res = []
+
+        for i in range(n):
+            left = 0
+            product = 1
+            while i>0 and left < i:
+                product *= nums[left]
+                left += 1
+            right = i+1
+            while right < n:
+                product *= nums[right]
+                right += 1
+            res.append(product)
+        
+        return res
+
+
+        
