e96d570e6c22bfd531a0c941eef5f879e8d0a337
RITU KUMARI <118442493+Ritukumari2003@users.noreply.github.com>
2026-04-04 15:52:51 +0530

    Clean up removeNthFromEnd method by removing comments
    Removed commented-out code for reverse_list and related logic.

--- Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.py
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.py
@@ -5,39 +5,8 @@
 #         self.next = next
 
 class Solution:
-    # def reverse_list(self, head):
-    #     prev = None
-    #     curr = head
-    #     while curr:
-    #         next = curr.next
-    #         curr.next = prev
-    #         prev = curr
-    #         curr = next
-    #     return prev
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # head = self.reverse_list(head)
-
-        # if not head:
-        #     return head
-        # if head.next is None:
-        #     if n == 1:
-        #         return None
-
-        # prev = None
-        # curr = head
-        # if n == 1:
-        #     head = head.next  
-        #     curr.next = None
-        #     return self.reverse_list(head) 
-        
-        # while curr and n-1!=0:
-        #     prev = curr
-        #     curr = curr.next
-        #     n -= 1
-        # prev.next = curr.next
-        # curr.next = None
-
-        # return self.reverse_list(head)
+        # Two pointer approach
         dummy = ListNode(0)
         dummy.next = head
 
