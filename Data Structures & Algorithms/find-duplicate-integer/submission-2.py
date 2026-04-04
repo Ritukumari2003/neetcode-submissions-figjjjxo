8583ca057b874b1444baadb7f7361ae2b9f4a4fc
RITU KUMARI <118442493+Ritukumari2003@users.noreply.github.com>
2026-04-05 01:26:53 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # Binary Search
+        low = 1
+        high = len(nums)-1
+
+        while low <= high:
+            mid = low + (high-low)//2
+            
+            count = 0
+            for num in nums:
+                if num <= mid:
+                    count += 1
+
+            if mid >= count:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
